9aac9f63d7f1e5db2045d505571bd794f4a2731b
HeliumCake <he-jl18@mails.tsinghua.edu.cn>
2020-10-26 20:50:10 +0800

    Update server.c

--- server.c
+++ server.c
@@ -133,7 +133,7 @@ int create_socket(char *ip, int port)
 void *communication(void *arg)
 {
     int connfd = *(int *)arg;
-    char *dir;
+    char dir[100];
     strcpy(dir, root_dir);
     char buffer[1024];
     memset(buffer, 0, 1024);
