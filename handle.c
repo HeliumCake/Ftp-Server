0ec383daf8da097b29d6ca0b7ac514f534efd539
HeliumCake <he-jl18@mails.tsinghua.edu.cn>
2020-10-27 01:58:53 +0800

    a

--- handle.c
+++ handle.c
@@ -174,7 +174,6 @@ void ftp_list(Command *cmd, int connfd, int datafd, char *dir)
 	int size;
 	while ((size = fread(buffer, 1, 100, f)) > 0)
 	{
-		printf("size=%d", size);
 		if (m_write(datafd, buffer, size) < 0)
 		{
 			char *reply = "426 Connection closed.\r\n";
