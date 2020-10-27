a2d9c18da151ac55e68badac38f114af888048e3
HeliumCake <he-jl18@mails.tsinghua.edu.cn>
2020-10-28 00:26:01 +0800

    Update handle.c

--- handle.c
+++ handle.c
@@ -264,11 +264,11 @@ void ftp_pwd(Command *cmd, int connfd, char *dir)
 
 void ftp_list(Command *cmd, int connfd, int datafd, char *dir)
 {
-	char filename[200];
+	char filename[210];
 	strcpy(filename, dir);
 	strcat(filename, "/");
 	strcat(filename, cmd->arg);
-	char name[200];
+	char name[220];
 	sprintf(name, "ls -al %s", filename);
 	FILE *f = popen(name, "r");
 	char buffer[100];
